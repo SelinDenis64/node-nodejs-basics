5694a8aa81d9eeb72669473e5fd6004f6adf05a5
SelinDenis64 <123951510+SelinDenis64@users.noreply.github.com>
2024-01-29 16:31:53 +0400

    feat: `create.js` solution

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,14 @@
-const create = async () => {
-    // Write your code here 
-};
+import path from "path";
+import {writeFile} from "fs/promises";
 
+const create = async () => {
+    const dir = path.resolve("./src/fs/files/fresh.txt");
+    try {
+        await writeFile(dir, "I am fresh and young", {
+            flag: "wx",
+        })
+    } catch (e) {
+        throw new Error("FS operation failed")
+    }
+};
 await create();
